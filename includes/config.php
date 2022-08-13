4be90cbd56b46247d21f794d8cfde635d9e3495c
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-08-13 11:50:50 +0200

    [Update] Global

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = [
+	"title" => "NothingElse.fr",
+	"title_admin" => "NothingElse.fr - Admin",
+	"title_admin" => "NothingElse.fr - Admin",
+	"color" => "#1abc9c",
+	"color_admin" => "#1abc9c",
+];
